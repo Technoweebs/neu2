9d081bfd65d69f43c39879268b173e233438e81a
TomaruDev <tomaru.development@gmail.com>
2020-12-04 19:20:31 +0100

    Fix NPM Package Name Regex

--- src/modules/parse.js
+++ src/modules/parse.js
@@ -14,7 +14,7 @@ module.exports = {
 			let package = JSON.parse(fs.readFileSync(options.dir + "/package.json"));
 			let settings = JSON.parse(fs.readFileSync(options.dir + "/app/settings.json"));
 			
-			package.name = options.project.toLowerCase().replace(/[^\<>,;:\.*\[\]\(\)+?¿!&%@=]/gi, '').replace(/\s/g, '-');
+			package.name = options.project.toLowerCase().replace(/[\^'"\<>,;:\.*\[\]\(\)+?¿!&%@=]/gi, '').replace(/\s/g, '-');
 			package.license = options.license;
 
 			package.scripts = {...{
